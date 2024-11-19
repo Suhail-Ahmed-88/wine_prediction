1c341a82692056e71f60c6f68fda9fd91e7fbb6c
Suhail-Ahmed-88 <rajpar.suhail.ahmed@gmail.com>
2024-11-19 11:33:35 +0800

    data_transformation and model_trainer added

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 from src.wine import logger
 from src.wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.wine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.wine.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.wine.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # logger.info("Welcome to Custom Logging")
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -17,12 +18,35 @@ except Exception as e:
     raise e
 
 
-Stage_name = "Data Validation stage"
+STAGE_NAME = "Data Validation Stage"
 try:
-    logger.info(f">>>>>> stage {Stage_name} started <<<<<<")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
     obj.main()
-    logger.info(f">>>>>> stage {Stage_name} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
